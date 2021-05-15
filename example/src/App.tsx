bfcaf3f2683c54b7ba732084f61fbf64ab3e1f2e
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-15 12:42:51 -0300

    compilation things

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from '@9gustin/react-notion-render'
+import { Heading1 } from '@9gustin/react-notion-render'
 import '@9gustin/react-notion-render/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Heading1 />
 }
 
 export default App
