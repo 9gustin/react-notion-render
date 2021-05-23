b400204d0b0c632870aea214dc3fed09a10c50c9
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-23 15:26:30 -0300

    use implicitly Block type

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import {render} from '@9gustin/react-notion-render'
+import {render, Block} from '@9gustin/react-notion-render'
 import '@9gustin/react-notion-render/dist/index.css'
 
 import mockedData from './mockedData.json'
 
 const App = () => {
-  return <div>{render(mockedData.results, true)}</div>
+  return <div>{render(mockedData.results as Block[], true)}</div>
 }
 
 export default App
