d6c9443729a363d91c868395b3b0055c1685c4b6
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-28 18:50:49 -0300

    modify mockedData and exports

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,19 @@ import Heading2 from './components/Heading2'
 import Heading3 from './components/Heading3'
 import List from './components/List'
 import Paragraph from './components/Paragraph'
+import StyledText from './components/StyledText'
 import { parseBlocks } from './utils/parseBlocks'
 import renderBlocks from './utils/renderBlocks'
 import Block from './types/Block'
+import Text from './types/Text'
 
 import './styles/index.css'
 
 // Components
-export { Heading1, Heading2, Heading3, Paragraph, List }
+export { Heading1, Heading2, Heading3, Paragraph, List, StyledText }
 
 // Utils
 export { renderBlocks as render, parseBlocks as parser }
 
 // Types
-export { Block }
+export { Block, Text }
