549b3049f28a289d18cd005cf70f6ea531dafcdd
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-06-26 13:14:16 -0300

    ad getClassName fnm

---
+++ src/utils/getClassname.ts
@@ -0,0 +1,14 @@
+import Text from "../types/Text";
+
+const DEFAULT_COLOR = 'default'
+
+export function getClassname(annotations: Text['annotations']) {
+  return `
+  ${annotations.bold ? 'rnr-bold' : ''}
+  ${annotations.code ? 'rnr-code' : ''}
+  ${annotations.italic ? 'rnr-italic' : ''}
+  ${annotations.strikethrough ? 'rnr-strikethrough' : ''}
+  ${annotations.underline ? 'rnr-underline' : ''}
+  ${annotations.color !== DEFAULT_COLOR ? `rnr-${annotations.color}` : ''}
+`.trim()
+}
