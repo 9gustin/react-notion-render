fe21a7533df02684459705951c75e440860d9c74
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-06-26 13:32:00 -0300

    improve links redirection

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -7,8 +7,11 @@ export interface Props {
 }
 
 function Link({url, content, className}: Props) {
+  const redirectProps = url.startsWith('#') ? {} : {
+    target: '_blank', rel:'noreferrer' 
+  }
   return (
-    <a href={url} target='_blank' rel='noreferrer' className={className}>
+    <a href={url} className={className} {...redirectProps}>
       {content}
     </a>
   )
