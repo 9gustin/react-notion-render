557c1b04303a9e5b999c734a880472bbf3a862d3
mrzmyr <moritz@devmood.com>
2021-12-15 12:39:40 +0100

    feat: add semantics to Text component

--- src/components/core/Text/index.tsx
+++ src/components/core/Text/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactElement } from 'react'
 
 import Text from '../../../types/Text'
 import { getClassname } from '../../../utils/getClassname'
@@ -7,35 +7,37 @@ import Link from '../../common/Link'
 import withCustomComponent from '../../../hoc/withCustomComponent'
 
 function Text({ text, annotations, type, href, plain_text }: Text) {
-  const cn = getClassname(annotations)
+  const className = getClassname(annotations)
 
   if (type === 'mention') {
     return (
-      <a className={`rnr-mention ${cn}`} href={href}>
+      <a className={`rnr-mention ${className}`} href={href}>
         {plain_text}
       </a>
     )
-  } else if (!text) {
-    return null
   }
 
-  const renderText = text.link
-    ? (
-    <Link url={text.link.url} className={cn}>
-      {text.content}
-    </Link>
-      )
-    : (
-        text.content
-      )
+  if (!text) return null
 
-  return cn
-    ? (
-    <span className={cn}>{renderText}</span>
-      )
-    : (
-    <Fragment>{renderText}</Fragment>
-      )
+  let element: ReactElement = <Fragment>{text.content}</Fragment>
+
+  if (className) element = <span className={className}>{text.content}</span>
+
+  if (annotations.bold) {
+    element = <strong className={className}>{text.content}</strong>
+  } else if (annotations.code) {
+    element = <code className={className}>{text.content}</code>
+  } else if (annotations.italic) {
+    element = <em className={className}>{text.content}</em>
+  } else if (annotations.strikethrough) {
+    element = <s className={className}>{text.content}</s>
+  } else if (annotations.underline) {
+    element = <u className={className}>{text.content}</u>
+  }
+
+  if (text.link) element = <Link url={text.link.url} className={className}>{element}</Link>
+
+  return element
 }
 
 export default withCustomComponent(Text)
