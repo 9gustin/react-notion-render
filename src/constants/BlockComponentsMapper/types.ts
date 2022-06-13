154e094e9477b5dada03358e2cecf695c06bb4d3
Agustin Vazquez <agustinvazquez453@gmail.com>
2022-06-13 13:03:38 -0300

    make optional block types

--- src/constants/BlockComponentsMapper/types.ts
+++ src/constants/BlockComponentsMapper/types.ts
@@ -4,27 +4,27 @@ import { WithContentValidationProps } from '../../hoc/withContentValidation'
 type BlockComponent = React.FC<WithContentValidationProps> | undefined
 
 export type BlockComponentsMapperType = {
-  [blockEnum.PARAGRAPH]: BlockComponent
-  [blockEnum.HEADING1]: BlockComponent
-  [blockEnum.HEADING2]: BlockComponent
-  [blockEnum.HEADING3]: BlockComponent
-  [blockEnum.DOTS_LIST]: BlockComponent
-  [blockEnum.ENUM_LIST]: BlockComponent
-  [blockEnum.CHECK_LIST]: BlockComponent
-  [blockEnum.TOGGLE_LIST]: BlockComponent
-  [blockEnum.VIDEO]: BlockComponent
-  [blockEnum.FILE]: BlockComponent
-  [blockEnum.PDF]: BlockComponent
-  [blockEnum.EMBED]: BlockComponent
-  [blockEnum.TITLE]: BlockComponent
-  [blockEnum.IMAGE]: BlockComponent
-  [blockEnum.CALLOUT]: BlockComponent
-  [blockEnum.QUOTE]: BlockComponent
-  [blockEnum.DIVIDER]: BlockComponent
-  [blockEnum.CODE]: BlockComponent
-  [blockEnum.TABLE_OF_CONTENTS]: BlockComponent
-  [blockEnum.TABLE]: BlockComponent
-  [blockEnum.TABLE_ROW]: BlockComponent
-  [blockEnum.SYNCED_BLOCK]: BlockComponent
-  [blockEnum.BOOKMARK]: BlockComponent
+  [blockEnum.PARAGRAPH]?: BlockComponent
+  [blockEnum.HEADING1]?: BlockComponent
+  [blockEnum.HEADING2]?: BlockComponent
+  [blockEnum.HEADING3]?: BlockComponent
+  [blockEnum.DOTS_LIST]?: BlockComponent
+  [blockEnum.ENUM_LIST]?: BlockComponent
+  [blockEnum.CHECK_LIST]?: BlockComponent
+  [blockEnum.TOGGLE_LIST]?: BlockComponent
+  [blockEnum.VIDEO]?: BlockComponent
+  [blockEnum.FILE]?: BlockComponent
+  [blockEnum.PDF]?: BlockComponent
+  [blockEnum.EMBED]?: BlockComponent
+  [blockEnum.TITLE]?: BlockComponent
+  [blockEnum.IMAGE]?: BlockComponent
+  [blockEnum.CALLOUT]?: BlockComponent
+  [blockEnum.QUOTE]?: BlockComponent
+  [blockEnum.DIVIDER]?: BlockComponent
+  [blockEnum.CODE]?: BlockComponent
+  [blockEnum.TABLE_OF_CONTENTS]?: BlockComponent
+  [blockEnum.TABLE]?: BlockComponent
+  [blockEnum.TABLE_ROW]?: BlockComponent
+  [blockEnum.SYNCED_BLOCK]?: BlockComponent
+  [blockEnum.BOOKMARK]?: BlockComponent
 }
